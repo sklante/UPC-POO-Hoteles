073913311dfe3915293c8177a0fab582c7fdbd7c
Miguel Angel <sklante_1@hotmail.com>
2012-06-10 13:53:49 -0500

    Actualizando

--- Avance Miguel/AdministracionHoteles/test/controladoras/HotelesTest.java
+++ Avance Miguel/AdministracionHoteles/test/controladoras/HotelesTest.java
@@ -20,8 +20,29 @@ public class HotelesTest {
     @Test
     public void siNoIngesoElNombreDelHotelDebeMostarunaListaDeNombresSugeridos() {
 
-        assertTrue(Hotel.validarHotel("Los Perdidos", "losperdidos.clerk.lm"));
+       /* assertTrue(Hotel.validarHotel("Los Perdidos", "losperdidos.clerk.lm"));
         System.out.println("nombre del hotel correcto");
+        * */
+        String nameHotel = "Los Perdidos";
+        
+        String recibeNombres[] = new String[5];
+        
+         Hotel hotel = new Hotel("Los Perdidos","losperdidos.clerk.lm");
+      //  Hotel hotel = new Hotel("Hotel rivera", "http://hotelrivera.clerk.im", "Alberto Fernandez");
+
+        recibeNombres = hotel.sugerirNombres(nameHotel);
+        //recibeNombres = null; cuando no hay valores falla
+        assertNotNull(recibeNombres);
+        System.out.println("Se sugirió los nombres: ");
+        for(int i = 0; i < recibeNombres.length; i++){
+            System.out.println((i+1) + ".- " + recibeNombres[i]);
+        }
+        System.out.println("se seleccionó el nombre " + recibeNombres[2]);
+        String nombreWeb = hotel.asignarLink(recibeNombres[2]);
+        assertNotNull(nombreWeb);
+
+        System.out.println(nombreWeb);
+        
     }
 
   
